ae677bd6df88dd583e96f7048b5e03660979a5bd
Alejandro Leon <alejandroleonmunoz1007@gmail.com>
2020-11-17 18:52:42 +0100

    codigo bien hecho

--- api-server/src/index.js
+++ api-server/src/index.js
@@ -10,10 +10,14 @@ const port = 3000;
 const indexRoutes = require('./routes/index')
 const empleadosRoutes = require('./routes/empleados')
 const empresasRoutes = require('./routes/empresas')
+const jornadasRoutes = require('./routes/jornadas')
+const fichajesRoutes = require('./routes/fichajes')
 // Routes
 app.use('/', indexRoutes)
 app.use('/empleados', empleadosRoutes)
 app.use('/empresas', empresasRoutes)
+app.use('/jornadas', jornadasRoutes)
+app.use('/fichajes', fichajesRoutes)
 
 //setting
 app.set('views', path.join(__dirname, 'views'))
